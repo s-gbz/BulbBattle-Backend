9a686cf05074f0fbb9ddcae9e1485692223fb0b6
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-06 23:46:13 +0200

    Enable CrossOrigin

--- bulbbattle-java/src/main/java/de/trzpiot/bulbbattle/BulbbattleApplication.java
+++ bulbbattle-java/src/main/java/de/trzpiot/bulbbattle/BulbbattleApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class BulbbattleApplication {
 
 	public static void main(String[] args) {
