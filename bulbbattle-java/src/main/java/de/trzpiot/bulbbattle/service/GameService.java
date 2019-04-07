1c16a9d86586b26935fdd2fe1243ab283cdadc08
Sebastian Trzpiot <mail@trzpiot.de>
2019-04-07 18:43:19 +0200

    Add the game logic to Java

--- bulbbattle-java/src/main/java/de/trzpiot/bulbbattle/service/GameService.java
+++ bulbbattle-java/src/main/java/de/trzpiot/bulbbattle/service/GameService.java
@@ -15,12 +15,20 @@ import java.util.concurrent.TimeUnit;
 public class GameService {
     private final Environment environment;
     private final LightService lightService;
+    private final String BRIDGE_IP;
+    private final String BRIDGE_USERNAME;
+    private final long LIGHT_ID;
+    private final long REQUIRED_TIME_OF_LIGHT_TO_CHANGE_STATUS = 2;
     private boolean running = false;
 
     @Autowired
     public GameService(Environment environment, LightService lightService) {
         this.environment = environment;
         this.lightService = lightService;
+
+        BRIDGE_IP = environment.getProperty("bridge.ip");
+        BRIDGE_USERNAME = environment.getProperty("bridge.username");
+        LIGHT_ID = Long.parseLong(Objects.requireNonNull(environment.getProperty("bridge.light.id")));
     }
 
     public void start(int rounds) {
@@ -41,19 +49,70 @@ public class GameService {
     }
 
     private void run(int rounds) throws InterruptedException {
-        final String bridgeIp = environment.getProperty("bridge.ip");
-        final String bridgeUsername = environment.getProperty("bridge.username");
-        final long lightId = Long.parseLong(Objects.requireNonNull(environment.getProperty("bridge.light.id")));
         int currentRound = 1;
 
+        startGame();
+        TimeUnit.SECONDS.sleep(10 + REQUIRED_TIME_OF_LIGHT_TO_CHANGE_STATUS);
+
         for (; currentRound <= rounds; currentRound++) {
-            lightService.setBrightness(bridgeIp, bridgeUsername, lightId, 255);
-            lightService.switchOn(bridgeIp, bridgeUsername, lightId);
-            TimeUnit.SECONDS.sleep(1);
-            lightService.switchOff(bridgeIp, bridgeUsername, lightId);
+            int[] colorSequence = getColorSequence(currentRound);
+
+            for (int colorCode : colorSequence) {
+                lightOn();
+                changeColor(colorCode);
+                TimeUnit.MILLISECONDS.sleep(getRoundDuration(currentRound));
+                lightOff();
+            }
+
+            endRound();
+            TimeUnit.SECONDS.sleep(15 + REQUIRED_TIME_OF_LIGHT_TO_CHANGE_STATUS);
         }
 
+        endGame();
         running = false;
+    }
+
+    private void startGame() {
+        lightService.switchOn(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
+        lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255, 255, 255);
+        lightService.setBrightness(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255);
+    }
+
+    private void endGame() {
+        lightService.switchOff(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
+    }
+
+    private void endRound() {
+        lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255, 255, 255);
+        lightService.setBrightness(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255);
+    }
+
+    private void changeColor(int colorCode) {
+        switch (colorCode) {
+            case 0:
+                lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 244, 67, 54);
+                break;
+            case 1:
+                lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 76, 175, 80);
+                break;
+            case 2:
+                lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 33, 150, 243);
+                break;
+            case 3:
+                lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255, 235, 59);
+                break;
+            default:
+                lightService.setColor(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID, 255, 255, 255);
+                break;
+        }
+    }
+
+    private void lightOn() {
+        lightService.switchOn(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
+    }
+
+    private void lightOff() {
+        lightService.switchOff(BRIDGE_IP, BRIDGE_USERNAME, LIGHT_ID);
     }
 
     private int[] getColorSequence(int currentRound) {
@@ -67,6 +126,6 @@ public class GameService {
     }
 
     private Long getRoundDuration(int currentRound) {
-        return 1500000L - (currentRound - 1) * 100000;
+        return 1500L - (currentRound - 1) * 100;
     }
 }
